62a056b77dee759fbc3d18f5505550e1d8936a5e
LawrenceChanKokWai <kokwai2107@gmail.com>
2024-07-04 14:03:32 +0800

    initial setup

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author chankokwai
+ * @project Default (Template) Project
+ * @date 4/7/24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
